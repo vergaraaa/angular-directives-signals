b4356b09a58179983a58fe103a9c39daa35e2618
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 12:31:38 +0200

    ✨ errors directive

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,3 +1,4 @@
+import { ValidationErrors } from '@angular/forms';
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
@@ -5,15 +6,21 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class CustomLabelDirective implements OnInit {
   // private htmlElemet?: ElementRef<HTMLElement>;
+  constructor(private el: ElementRef<HTMLElement>) {
+    // this.htmlElemet = el;
+  }
+
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
   }
 
-  constructor(private el: ElementRef<HTMLElement>) {
-    // this.htmlElemet = el;
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   ngOnInit(): void {
@@ -24,5 +31,35 @@ export class CustomLabelDirective implements OnInit {
     this.el.nativeElement.style.color = this._color;
     // if (!this.htmlElemet) return;
     // this.htmlElemet!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    // if (!this.htmlElemet) return;
+    // this.htmlElemet!.nativeElement.style.color = this._color;
+
+    if (!this._errors) {
+      this.el.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.el.nativeElement.innerText = 'This field is required.';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.el.nativeElement.innerText = `Min ${current}/${min} characters.`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.el.nativeElement.innerText = 'This field must be an email.';
+      return;
+    }
   }
 }
