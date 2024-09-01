59f35ee1c4749df5d86a12863875d40baa8715cb
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 18:26:00 +0200

    ✨ effect signal

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
+  public counter = signal(13);
 
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
+
+  public fullname = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  public userChangedEffect = effect(() => {
+    console.log(`${this.user().first_name} - ${this.counter}`);
+  });
+
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+      }
+
+      return { ...current };
+    });
+  }
+
+  increaseBy(value: number): void {
+    this.counter.update((current) => current + value);
+  }
 }
