836676740384c1acff0beb82d6be59fefe7a65e0
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 16:58:05 +0200

    ✨ signals side menu

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'signals-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css',
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    {
+      title: 'Counter',
+      route: 'counter',
+    },
+    {
+      title: 'User Info',
+      route: 'user-info',
+    },
+    {
+      title: 'Mutations',
+      route: 'properties',
+    },
+  ];
+}
--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -6,13 +6,15 @@ import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.c
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     SignalsLayoutComponent,
     CounterPageComponent,
     UserInfoPageComponent,
-    PropertiesPageComponent
+    PropertiesPageComponent,
+    SideMenuComponent
   ],
   imports: [CommonModule, SignalsRoutingModule],
 })
