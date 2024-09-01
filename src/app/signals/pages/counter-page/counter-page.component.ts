806666aead51e9a5473ceca3710a69c20a60fa7e
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 12:44:54 +0200

    ✨ signals module structure

---
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styleUrl: './counter-page.component.css'
+})
+export class CounterPageComponent {
+
+}
