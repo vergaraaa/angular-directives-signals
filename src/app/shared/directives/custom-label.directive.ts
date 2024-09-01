be7808c81530a1157f8578ba46a0fbe5f40c9d11
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 12:07:51 +0200

    ✨ directive with color input

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,10 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
 })
-export class CustomLabelDirective {
+export class CustomLabelDirective implements OnInit {
+  // private htmlElemet?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
   constructor(private el: ElementRef<HTMLElement>) {
-    this.el.nativeElement.innerHTML = 'hello world';
+    // this.htmlElemet = el;
+  }
+
+  ngOnInit(): void {
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    this.el.nativeElement.style.color = this._color;
+    // if (!this.htmlElemet) return;
+    // this.htmlElemet!.nativeElement.style.color = this._color;
   }
 }
