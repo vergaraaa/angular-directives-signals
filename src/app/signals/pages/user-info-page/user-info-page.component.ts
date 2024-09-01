fc27b43802fc61b786c74b6969c739ca7a0526b0
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-01 17:40:22 +0200

    ✨ user signal

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { UsersService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
+import { filter } from 'rxjs';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersService = inject(UsersService);
 
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+  public fullname = computed<string>(() => {
+    if (!this.currentUser()) return 'User not found';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+  ngOnInit(): void {
+    this.getUser(this.userId());
+  }
+
+  getUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.currentUser.set(undefined);
+        this.userWasFound.set(false);
+      },
+    });
+  }
 }
